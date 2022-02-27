f291bf3f2f93aea088c13a12ce4fb9b7e90f1864
Dave Richardson <contactme@daverichardson.ca>
2022-02-27 16:46:10 -0600

    style: cleans up some styling in the header component
    Removes some unused variables

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,10 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import ClockIcon from '@mui/icons-material/DepartureBoard';
 import MenuIcon from '@mui/icons-material/Menu';
 import styled from "@emotion/styled";
-
-const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
 
 export default function Header() {
     return (
