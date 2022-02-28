68b3388c4719013d5de20e3d08d1952c496c52a1
Dave Richardson <contactme@daverichardson.ca>
2022-02-27 21:08:19 -0600

    feat(stopmap): adds markers to the map for stops near a given location
    Removed the winnipegtransitapi package as it has a bug that needs to be fixed.

--- src/api/stops.js
+++ src/api/stops.js
@@ -1,12 +1,16 @@
-import WinnipegTransitAPI from "winnipegtransitapi";
+// import WinnipegTransitAPI from "winnipegtransitapi";
 
-const api_client = new WinnipegTransitAPI(process.env.REACT_APP_TRANSIT_API_KEY);
+// const api_client = new WinnipegTransitAPI(process.env.REACT_APP_TRANSIT_API_KEY);
 
 export const getNearbyStops = async ({ lat, lon, distance = 1000 }) => {
     try {
-        return await api_client.getStopsMatching({ lat, lon, distance });
+        // return await api_client.getStopsMatching({ lat, lon, distance });
+        const reqUrl = `https://api.winnipegtransit.com/v3/stops.json?api-key=${process.env.REACT_APP_TRANSIT_API_KEY}&lat=${lat}&lon=${lon}&distance=${distance}`
+        const response = await fetch(reqUrl);
+        return response.json();
     } catch (e) {
         console.log("Exception Caught => ", e);
+        throw e;
     }
 }
 
--- src/components/StopMap.js
+++ src/components/StopMap.js
@@ -1,15 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import StopsAPI from "../api/stops";
 
-// import StopsAPI from "../api/stops";
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+// const StopIcon = (
+//   L.Icon({
+//     iconUrl: markerIcon,
+//     shadowUrl: iconShadow,
+//   })
+// );
 
 export const StopMap = (props) => {
     const mapPositions = [49.8952, -97.1379];
-    const [ stopPositions, setStopPositions ] = useState([]);
+    const [ nearbyStops, setNearbyStops ] = useState([]);
 
-    useEffect(() => {
-        console.log("Fetch nearby stops => ", mapPositions);
+    useEffect(async () => {
+        const stops_nearby = await StopsAPI.getNearbyStops({lat: mapPositions[0], lon: mapPositions[1], distance: 1000 });
+        const { stops = [] } = stops_nearby;
+
+        setNearbyStops(stops);
     }, [mapPositions]);
 
     return (
@@ -22,6 +40,23 @@ export const StopMap = (props) => {
                 attribution={'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}
                 url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
             />
+            {(nearbyStops.map((nearbyStop) => {
+                const { centre = {}, name, key } = nearbyStop;
+                const { geographic = {} } = centre;
+                const { latitude, longitude } = geographic;
+
+                return (
+                  <Marker key={key}
+                          position={[latitude, longitude]}
+                          // icon={<StopIcon />}
+                  >
+                    <Popup>
+                        {key}: {name}
+                        <br />
+                    </Popup>
+                  </Marker>
+                );
+            }))}
         </MapContainer>
     )
 }
